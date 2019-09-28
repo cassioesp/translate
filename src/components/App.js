5fcbc870bdff2c1d75b20bf7cff87eac26fe312a
Cássio Espindola <cassioafonso@gmail.com>
2019-09-28 14:05:24 -0300

    Adding components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 import '../App.css';
 
 class App extends React.Component {
 
     state = {language: 'english'};
 
+    onLanguageChange = language => {
+        this.setState({language})
+    };
+
     render() {
         return (
-            <div className="ui container">App
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
+                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
+
+                </div>
+                <UserCreate/>
             </div>
-        );
+        )
     }
 }
 
