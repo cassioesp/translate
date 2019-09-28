0ccc82e6d957baafa2b7df1bae675ace906aa789
Cássio Espindola <cassioafonso@gmail.com>
2019-09-28 13:53:47 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+class App extends React.Component {
+
+    state = {language: 'english'};
+
+    render() {
+        return (
+            <div className="ui container">App
+            </div>
+        );
+    }
+}
+
+export default App;
